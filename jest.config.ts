664406ca203c0551ede3d14263d3ebffe108bbce
aac-dot <ayrton.andrade1@gmail.com>
2023-03-03 16:09:12 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
